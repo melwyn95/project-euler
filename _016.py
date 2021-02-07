5f0795b14cc4f977a12f5923198361135c7405ab
Melwyn Saldanha <melwyn95@gmail.com>
2021-02-07 13:11:00 +0530

    reuse big_add from PE-16

--- _016.py
+++ _016.py
@@ -1,31 +1,7 @@
 import functools
 
-def big_add(a, b):
-    c = 0
-    an, bn = len(a) - 1, len(b) - 1
-    x = []
-    while an >= 0 and bn >= 0:
-        s = a[an] + b[bn] + c
-        if s > 9: c = s // 10
-        else: c = 0
-        x.append(s % 10)
-        an -= 1
-        bn -= 1
-    while an >= 0:
-        s = a[an] + c
-        if s > 9: c = s // 10
-        else: c = 0
-        x.append(s % 10)
-        an -= 1
-    while bn >= 0:
-        s = b[bn] + c
-        if s > 9: c = s // 10
-        else: c = 0
-        x.append(s % 10)
-        bn -= 1 
-    if c > 0: x.append(c)
-    return list(reversed(x))
- 
+from _013 import big_add
+
 def big_multiply(a, b):
     p = []
     for z, bi in enumerate(reversed(b)):
