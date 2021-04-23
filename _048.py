73ef130bef2554b5e808746072092601f347e2d4
Melwyn Saldanha <melwyn95@gmail.com>
2021-04-23 20:36:13 +0530

    slightly better solution for PE-48

--- _048.py
+++ _048.py
@@ -1,18 +1,54 @@
-from utils import digits, big_multiply, digits_to_int
+from utils import digits, digits_to_int
 
 N = 1000
-mod = 10 ** 10
 
-def big_pow(n, x):
-    ds = digits(n)
-    acc = digits(n)
-    for _ in range(x - 1):
-        acc = big_multiply(acc, ds)
-    return digits_to_int(acc) % mod
+def trunc_add(xys):
+    zs = []
+    n = max(map(len, xys))
+    carry = 0
+    bail = False
+    for i in range(n):
+        t = carry
+        for ys in xys:
+            if i < len(ys): t += ys[i]
+        if t >= 10: carry = t // 10
+        else: carry = 0
+        zs.append(t % 10)
+        if len(zs) == 10:
+            bail = True
+            break
+    if not bail:
+        if carry > 0: zs.append(carry)
+    return list(reversed(zs))
 
-s = 0
-for i in range(1, N + 1):
-    print(i)
-    s += big_pow(i, i)
+def trunc_multiply(xs, ys):
+    zs = []
+    for yi in range(len(ys) - 1, -1, -1):
+        bail = False
+        carry = 0
+        z = [0 for i in range(len(ys) - 1 - yi)]
+        for xi in range(len(xs) - 1, -1, -1):
+            t = (xs[xi] * ys[yi]) + carry
+            if t >= 0: carry = t // 10
+            else: carry = 0
+            z.append(t % 10)
+            if len(z) == 10:
+                zs.append(z)
+                bail = True
+                break
+        if not bail:
+            if carry > 0: z.append(carry)
+            zs.append(z)
+    return trunc_add(zs)
 
-print(s)
+def trunc_pow(x, n):
+    ys = digits(x)
+    xs = digits(x)
+    for _ in range(n - 1):
+        ys = trunc_multiply(ys, xs)
+    return ys
+
+pows = []
+for n in range(N + 1): pows.append(list(reversed(trunc_pow(n, n))))
+answer = digits_to_int(trunc_add(pows))
+print(answer == 9110846700)
