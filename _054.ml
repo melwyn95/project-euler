3671df72a93aad467a3ca2fff5de8b0671c9a000
Melwyn Saldanha <melwyn95@gmail.com>
2021-08-15 22:29:48 +0530

    slightly improve PE-54

--- _054.ml
+++ _054.ml
@@ -18,30 +18,50 @@ type rank = [
   | `FourKind of value
   | `StraightFlush
   | `RoyalFlush]
+
+(* let pp_value =
+  function
+    Value n -> "Value (" ^ string_of_int n ^ ") "
+  | Jack -> "Jack"
+  | King -> "King"
+  | Queen -> "Queen"
+  | Ace -> "Ace"
+
+let rec pp_rank (rank : rank) = 
+  match rank with
+    `HighCard v -> "HighCard (" ^ pp_value v ^ ") "
+  | `OnePair v -> "OnePair (" ^ pp_value v ^ ") "
+  | `TwoPairs (v1, v2) -> "TwoPairs (" (* ^ pp_rank v1 ^ ", " ^ pp_rank v2 ^ ") "*)
+  | `ThreeKind v -> "ThreeKind (" ^ pp_value v ^ ") "
+  | `Straight -> "Straight"
+  | `Flush -> "Flush"
+  | `FullHouse (v1, v2) -> "FullHouse (" (* ^ pp_rank v1 ^ ", " ^ pp_rank v2 ^ ") "*)
+  | `FourKind v -> "FourKind (" ^ pp_value v ^ ") "
+  | `StraightFlush -> "StraightFlush"
+  | `RoyalFlush -> "RoyalFlush" *)
   
 let compare_values x y = 
   match (x, y) with
   | (Value x), (Value y) -> if x > y then (Value x) else (Value y)
   | Jack, (Value _ | Jack)
   | (Value _), Jack -> Jack
-  | King, (Value _ | Jack | King)
-  | (Value _ | Jack), King  -> King
-  | Queen, (Value _ | Jack | King | Queen) 
-  | (Value _ | Jack | King), Queen-> Queen
-  | Ace, _ | _, Ace-> Ace 
+  | Queen, (Value _ | Jack | Queen)
+  | (Value _ | Jack), Queen  -> Queen
+  | King, (Value _ | Jack | Queen | King) 
+  | (Value _ | Jack | Queen), King -> King
+  | Ace, _ | _, Ace -> Ace 
 
 let are_cards_consecutive = 
   function 
-    [(_, Value 1); (_, Value 2); (_, Value 3); (_, Value 4); (_, Value 5)]
   | [(_, Value 2); (_, Value 3); (_, Value 4); (_, Value 5); (_, Value 6)]
   | [(_, Value 3); (_, Value 4); (_, Value 5); (_, Value 6); (_, Value 7)]
   | [(_, Value 4); (_, Value 5); (_, Value 6); (_, Value 7); (_, Value 8)]
   | [(_, Value 5); (_, Value 6); (_, Value 7); (_, Value 8); (_, Value 9)]
   | [(_, Value 6); (_, Value 7); (_, Value 8); (_, Value 9); (_, Value 10)]
   | [(_, Value 7); (_, Value 8); (_, Value 9); (_, Value 10); (_, Jack)]
-  | [(_, Value 8); (_, Value 9); (_, Value 10); (_, Jack); (_, King)]
-  | [(_, Value 9); (_, Value 10); (_, Jack); (_, King); (_, Queen)]
-  | [(_, Value 10); (_, Jack); (_, King); (_, Queen); (_, Ace)] -> true
+  | [(_, Value 8); (_, Value 9); (_, Value 10); (_, Jack); (_, Queen)]
+  | [(_, Value 9); (_, Value 10); (_, Jack); (_, Queen); (_, King)]
+  | [(_, Value 10); (_, Jack); (_, Queen); (_, King); (_, Ace)] -> true
   | _ -> false 
     
 let are_cards_same_suit =
@@ -52,9 +72,9 @@ let are_cards_same_suit =
   | [(Spade, _); (Spade, _); (Spade, _); (Spade, _); (Spade, _)] -> true
   | _ -> false
 
-let is_royal_flush = 
-  function 
-    [(_, Value 10); (_, Jack); (_, King); (_, Queen); (_, Ace)] 
+let is_royal_flush cards = 
+  match cards with 
+    [(_, Value 10); (_, Jack); (_, Queen); (_, King); (_, Ace)] -> are_cards_same_suit cards
   | _ -> false
 
 let high_card cards = 
@@ -83,15 +103,10 @@ let int_card_value =
       
 
 let rank_cards cards : rank = 
-  if are_cards_same_suit cards
-  then 
-    if is_royal_flush cards
-    then `RoyalFlush
-    else if are_cards_consecutive cards
-    then `StraightFlush
-    else `Flush
-  else if are_cards_consecutive cards
-  then `Straight
+  if is_royal_flush cards then `RoyalFlush
+  else if are_cards_same_suit cards && are_cards_consecutive cards then `StraightFlush
+  else if are_cards_same_suit cards then `Flush
+  else if are_cards_consecutive cards then `Straight
   else 
     let card_occs = List.sort occ_cmp  @@ card_occurances cards in
     match List.length card_occs with
@@ -103,19 +118,19 @@ let rank_cards cards : rank =
         )
     | 3 -> 
         (match card_occs with
-         | [_; (v1, 2); (v2, 2)] -> 
+         | [(_, 1); (v1, 2); (v2, 2)] -> 
              if int_card_value v1 > int_card_value v2
              then `TwoPairs (`OnePair v1, `OnePair v2)
              else `TwoPairs (`OnePair v2, `OnePair v1)
-         | [_; _; (v, 3)] -> `ThreeKind v
+         | [(_, 1); (_, 1); (v, 3)] -> `ThreeKind v
          | _ -> failwith "card_occs length: 3"
         )
     | 4 ->
         (match card_occs with
-         | [_; _; _; (v, 2)] ->  `OnePair v 
+         | [_; _; _; (v, 2)] -> `OnePair v 
          | _ -> failwith "card_occs length: 4"
         )
-    | 1 | 5 -> `HighCard (high_card @@ cards)
+    | 5 -> `HighCard (high_card cards)
     | _ -> failwith "card_occs invalid length";; 
         
 let separate xs = 
@@ -137,7 +152,7 @@ let parse_card c =
     | 'C' -> Club 
     | _  -> failwith "parse error: invalid suit" in
   let value = match c.[0] with
-    | '0' .. '9' -> Value (int_of_char c.[0] - 48)
+    | '2' .. '9' -> Value (int_of_char c.[0] - 48)
     | 'T' -> Value 10
     | 'K' -> King
     | 'Q' -> Queen
@@ -147,92 +162,67 @@ let parse_card c =
   in
   (suit, value)
   
+let print_list xs =
+  List.iter (fun x -> print_int x; print_string " ") xs;
+  print_newline ();;
 
 let rec compare_lists xs ys = 
   match (xs, ys) with
   | (x :: xs), (y :: ys) -> if x > y
       then true
+      else if x < y
+      then false
       else compare_lists xs ys
   | _ -> failwith "invalid: compare_lists"
   
 let compare_ranks r1 r2 xs ys = 
   match (r1, r2) with
-  | `RoyalFlush, `RoyalFlush -> 
-      compare_lists xs ys
-  | `RoyalFlush, _ -> true
-  | _, `RoyalFlush -> false 
-  | `StraightFlush, `StraightFlush ->
-      compare_lists xs ys
-  | `StraightFlush, _ -> true
-  | `FourKind v1, `FourKind v2 ->
-      if int_card_value v1 > int_card_value v2
-      then true
-      else if int_card_value v1 = int_card_value v2
-      then compare_lists xs ys
-      else false
-  | `FourKind _, `StraightFlush -> false
-  | `FourKind _, _ -> true
+  | `TwoPairs (`OnePair v1, `OnePair v2), `TwoPairs (`OnePair v3, `OnePair v4)
   | `FullHouse (`ThreeKind v1, `OnePair v2), `FullHouse (`ThreeKind v3, `OnePair v4) ->
-      if int_card_value v1 > int_card_value v3
+    if int_card_value v1 > int_card_value v3
+    then true
+    else if int_card_value v1 = int_card_value v3
+    then 
+      if int_card_value v2 > int_card_value v4
       then true
-      else if int_card_value v1 = int_card_value v2
-      then 
-        if int_card_value v2 > int_card_value v4
-        then true
-        else if int_card_value v2 = int_card_value v4
-        then compare_lists xs ys
-        else false
+      else if int_card_value v2 = int_card_value v4
+      then compare_lists xs ys
       else false
-  | `FullHouse _, (`StraightFlush | `FourKind _) -> false
-  | `FullHouse _, _ -> true
-  | `Flush, `Flush ->
-      compare_lists xs ys
-  | `Flush, (`StraightFlush | `FourKind _ | `FullHouse _) -> false
-  | `Flush, _ -> true
-  | `Straight, `Straight ->
-      compare_lists xs ys
-  | `Straight, 
-    (`StraightFlush | `FourKind _ | `FullHouse _ | `Flush) -> false
-  | `Straight, _ -> true
+    else false
+  | `RoyalFlush, `RoyalFlush 
+  | `StraightFlush, `StraightFlush
+  | `Flush, `Flush
+  | `Straight, `Straight -> 
+    compare_lists xs ys
+  | `OnePair v1, `OnePair v2
+  | `HighCard v1, `HighCard v2
+  | `FourKind v1, `FourKind v2
   | `ThreeKind v1, `ThreeKind v2 -> 
-      if int_card_value v1 > int_card_value v2
-      then true
-      else if int_card_value v1 = int_card_value v2
-      then compare_lists xs ys
-      else false
-  | `ThreeKind _, 
-    (`StraightFlush | `FourKind _ | `FullHouse _ | `Flush | `Straight) -> false
-  | `ThreeKind _, _ -> true
-  | `TwoPairs (`OnePair v1, `OnePair v2), `TwoPairs (`OnePair v3, `OnePair v4) -> 
-      if int_card_value v1 > int_card_value v3
-      then true
-      else if int_card_value v1 = int_card_value v2
-      then 
-        if int_card_value v2 > int_card_value v4
-        then true
-        else if int_card_value v2 = int_card_value v4
-        then compare_lists xs ys
-        else false
-      else false
-  | `TwoPairs _, 
-    (`StraightFlush | `FourKind _ | `FullHouse _ | `Flush | `Straight | `ThreeKind _) -> false
-  | `TwoPairs _, _ -> true
-  | `OnePair v1, `OnePair v2 -> 
-      if int_card_value v1 > int_card_value v2
-      then true
-      else if int_card_value v1 = int_card_value v2
-      then compare_lists xs ys
-      else false
-  | `OnePair _, 
-    (`StraightFlush | `FourKind _ | `FullHouse _ | `Flush | `Straight | `ThreeKind _ | `TwoPairs _) -> false
-  | `OnePair _, _ -> true
-  | `HighCard v1, `HighCard v2 -> 
-      if int_card_value v1 > int_card_value v2
-      then true
-      else if int_card_value v1 = int_card_value v2
-      then compare_lists xs ys
-      else false
+    if int_card_value v1 > int_card_value v2
+    then true
+    else if int_card_value v1 = int_card_value v2
+    then compare_lists xs ys
+    else false
+  
+  | _, `RoyalFlush
+  | `FourKind _, `StraightFlush
+  | `FullHouse _, (`FourKind _ | `StraightFlush) 
+  | `Flush, (`FullHouse _ | `FourKind _ | `StraightFlush) 
+  | `Straight, (`Flush | `FullHouse _ | `FourKind _ | `StraightFlush) 
+  | `ThreeKind _, (`Straight | `Flush | `FullHouse _ | `FourKind _ | `StraightFlush) 
+  | `TwoPairs _, (`ThreeKind _ | `Straight | `Flush | `FullHouse _ | `FourKind _ | `StraightFlush) 
+  | `OnePair _, (`TwoPairs _ | `ThreeKind _ | `Straight | `Flush | `FullHouse _ | `FourKind _ | `StraightFlush) 
   | `HighCard _, _ -> false
+
+  | `RoyalFlush, _
+  | `StraightFlush, _ 
+  | `FourKind _, _ 
+  | `FullHouse _, _ 
+  | `Flush, _
+  | `Straight, _ 
+  | `ThreeKind _, _
+  | `TwoPairs _, _ 
+  | `OnePair _, _ -> true
   
 let parse_line s = 
   let ss = String.split_on_char ' ' s in
@@ -240,18 +230,18 @@ let parse_line s =
   let first, second = separate cards in
   let rank_first, rank_second = rank_cards first, rank_cards second in
   let value_first, value_second =
-    List.map int_card_value @@ List.map snd first,
-    List.map int_card_value @@ List.map snd second in
+    List.rev @@ List.sort Stdlib.compare @@ List.map int_card_value @@ List.map snd first,
+    List.rev @@ List.sort Stdlib.compare @@ List.map int_card_value @@ List.map snd second in
   if compare_ranks rank_first rank_second value_first value_second
   then 1
   else 0;;
     
     
-(* parse_line "5H 5C 6S 7S KD 2C 3S 8S 8D TD";;
-parse_line "5D 8C 9S JS AC 2C 5C 7D 8S QH";;
-parse_line "2D 9C AS AH AC 3D 6D 7D TD QD";;
-parse_line "4D 6S 9H QH QC 3D 6D 7H QD QS";;
-parse_line "2H 2D 4C 4D 4S 3C 3D 3S 9S 9D";; *)
+assert (parse_line "5H 5C 6S 7S KD 2C 3S 8S 8D TD" = 0);;
+assert (parse_line "5D 8C 9S JS AC 2C 5C 7D 8S QH" = 1);;
+assert (parse_line "2D 9C AS AH AC 3D 6D 7D TD QD" = 0);;
+assert (parse_line "4D 6S 9H QH QC 3D 6D 7H QD QS" = 1);;
+assert (parse_line "2H 2D 4C 4D 4S 3C 3D 3S 9S 9D" = 1);;
 
 let read_file_lines filename =
   let ch = open_in filename in
